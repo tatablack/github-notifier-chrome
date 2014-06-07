b9b7d0d84ac7367b7fff6e5d2428532d3f817e7f
Angelo Tata <atata@ebay.com>
2014-06-08 01:41:53 +0200

    Added Options page. Web font from icomoon. Url checker.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -6,16 +6,16 @@ function getServers() {
 
 function retrieveNotifications(alarm) {
     if (alarm.name === 'retrieveNotifications') {
-        console.log('About to retrieve notifications');
+        console.log('github-notifier: about to retrieve notifications');
         
         _.each(getServers(), function(server) {
-            uxhr(server.retrieveNotifications, {
-                user: 'atata'
-            }, {
-                complete: function (response) {
-                    console.log('notifications retrieved')
-                }
-            });
+//            uxhr(server.retrieveNotifications, {
+//                user: 'atata'
+//            }, {
+//                complete: function (response) {
+//                    console.log('github-notifier: notifications retrieved')
+//                }
+//            });
         });
     }
 }
@@ -26,6 +26,9 @@ chrome.runtime.onStartup.addListener(function() {
 chrome.runtime.onInstalled.addListener(function (details) {
     console.log('github-notifier: starting up');
     
+    if (details.reason === 'update') {
+        console.log('github-notifier: just updated from version %s to %s', details.previousVersion, chrome.app.getDetails().version);
+    }
     chrome.browserAction.setBadgeText({text: '...'});
     
     chrome.alarms.create('retrieveNotifications', {
--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,3 +1,47 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Option');
+var UrlRegExp = /\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/i;
+var currentRequest;
+
+function checkAvailability(url) {
+    if (currentRequest) {
+        console.log('A request was already executing');
+        currentRequest.abort();
+    }
+    
+    currentRequest = $.ajax({
+        url: url + '/ping',
+        success: function(response) {
+            var json = $.parseJSON(response);
+            $('.availability').
+                removeClass('icon-cross').
+                addClass('icon-checkmark').
+                text('Github Listener v' + json.version + ' found');
+        },
+        error: function(status, statusText, responseText) {
+            $('.availability').
+                removeClass('icon-checkmark').
+                addClass('icon-cross').
+                text('Server not available');
+        },
+        complete: function() {
+            currentRequest = null;            
+        }
+    });
+}
+
+$(function() {
+    var debouncedCheckAvailability = _.debounce(checkAvailability, 200);
+    $('#listener').on('input', function() {
+        if (UrlRegExp.test(this.value)) {
+            debouncedCheckAvailability(this.value);
+        };
+    });
+});
+
+
+function isUrl(s) {
+	var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+	return regexp.test(s);
+}
+
