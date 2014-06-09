65572ea59a15710a72c3c027808791550934fd6c
Angelo Tata <atata@ebay.com>
2014-06-10 00:56:08 +0200

    Refactor options code to improve readability

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,122 +1,138 @@
-'use strict';
-
-var urlRegExp = /\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/i,
-    usernameRegExp = /^[a-zA-Z0-9]+[a-zA-Z0-9\-]*$/;
-
-var currentRequest;
-
-var extensionMessagesSuccess = humane.create({ baseCls: 'humane-jackedup', addnCls: 'humane-jackedup-success' }),
-    extensionMessagesError = humane.create({ timeout: 5000, clickToClose: true, baseCls: 'humane-jackedup', addnCls: 'humane-jackedup-error' });
-
-function checkAvailability(url) {
-    if (!url) {
-        $('.listener-validation').removeClass('icon-cross icon-checkmark');
-        $('.listener-validation-message').text('');
-        return;
-    }
+/*jshint unused:false */
+/*jshint sub:true */
+/*global chrome, humane, self, ChromeStorage */
+var Options = (function() {
+    'use strict';
     
-    if (currentRequest) {
-        console.log('A request was already executing');
-        currentRequest.abort();
-    }
+    var urlRegExp = /\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/i,
+        usernameRegExp = /^[a-zA-Z0-9]+[a-zA-Z0-9\-]*$/;
     
-    currentRequest = $.ajax({
-        url: url + '/v1',
-        success: function(response) {
-            $('.listener-validation').
-                removeClass('icon-cross').
-                addClass('icon-checkmark');
-            
-            $('.listener-validation-message').text('Github Listener v' + response.server.version + ' found');
-        },
-        error: function() {
-            $('.listener-validation').
-                removeClass('icon-checkmark').
-                addClass('icon-cross');
-            
-            $('.listener-validation-message').text('Server not available');
-        },
-        complete: function() {
-            currentRequest = null;
-        }
-    });
-}
-
-function getOptions() {
-    var options = {
-        'username': $('#username').val()
+    var currentRequest;
+    
+    var extensionMessagesSuccess = humane.create({ baseCls: 'humane-jackedup', addnCls: 'humane-jackedup-success' }),
+        extensionMessagesError = humane.create({ timeout: 5000, clickToClose: true, baseCls: 'humane-jackedup', addnCls: 'humane-jackedup-error' });
+    
+    
+    var clearField = function(fieldName) {
+        $('.' + fieldName).removeClass('icon-cross icon-checkmark');
+        $('.' + fieldName + '-message').text('');
     };
     
-    if ($('.listener-validation').hasClass('icon-checkmark')) {
-        options['listener'] = $('#listener').val();
-    }
-    
-    return options;
-}
-
-function initOptions() {
-    Promise.all([ChromeStorage.read('username'), ChromeStorage.read('listener')]).then(function(results) {
-        $('#username').val(results[0]);
-        checkUsernameValidity(results[0]);
-
-        $('#listener').val(results[1]);
-        checkAvailability(results[1]);
-    });
-}
-
-function checkUsernameValidity(username) {
-    if (!username) {
-        $('.username-validation').removeClass('icon-cross icon-checkmark');
-        $('.username-validation-message').text('');
-        return;
-    }
-    
-    if (usernameRegExp.test(username)) {
-        $('.username-validation').
-            removeClass('icon-cross').
-            addClass('icon-checkmark');
-
-        $('.username-validation-message').text('This seems a valid GitHub username');
-    } else {
-        $('.username-validation').
-            removeClass('icon-checkmark').
-            addClass('icon-cross');
-
-        $('.username-validation-message').text('Invalid username');
-        
-        extensionMessagesError.log('GitHub usernames may only contain alphanumeric<br>characters or dashes and cannot begin with a dash');
-    }
-}
-
-$(function() {
-    var debouncedCheckAvailability = _.debounce(checkAvailability, 200);
-    
-    initOptions();
-    
-    $('#username').on('input', function() {
-        checkUsernameValidity(this.value);
-    });
-    
-    $('#listener').on('input', function() {
-        if (urlRegExp.test(this.value)) {
-            debouncedCheckAvailability(this.value);
-        } else {
-            $('.listener-validation').removeClass('icon-cross icon-checkmark');
-            $('.listener-validation-message').text('');            
+    var checkListenerAvailability = function(url) {
+        if (!url) {
+            clearField('listener-validation');
+            return;
         }
-    });
-    
-    $('#saveButton').on('click', function(evt) {
-        evt.preventDefault();
-
-        chrome.runtime.sendMessage({ command: 'retrieveNotifications'});
         
-        ChromeStorage.save(getOptions(), function() {
+        if (currentRequest) {
+            console.log('A request was already executing');
+            currentRequest.abort();
+        }
+        
+        currentRequest = $.ajax({
+            url: url + '/v1',
+            success: function(response) {
+                $('.listener-validation').
+                    removeClass('icon-cross').
+                    addClass('icon-checkmark');
+                
+                $('.listener-validation-message').text('Github Listener v' + response.server.version + ' found');
+            },
+            error: function() {
+                $('.listener-validation').
+                    removeClass('icon-checkmark').
+                    addClass('icon-cross');
+                
+                $('.listener-validation-message').text('Server not available');
+            },
+            complete: function() {
+                currentRequest = null;
+            }
+        });
+    };
+    
+    var saveOptions = function() {
+        var options = {
+            username: $('#username').val(),
+            listener: $('#listener').val()
+        };
+
+        ChromeStorage.save(options, function() {
             extensionMessagesSuccess.log('Options saved');
         });
-    });
+    };
+    
+    var initOptions = function() {
+        Promise.all([
+            ChromeStorage.read('username'),
+            ChromeStorage.read('listener')
+        ]).then(function(results) {
+            $('#username').val(results[0]);
+            checkUsernameValidity(results[0]);
+    
+            $('#listener').val(results[1]);
+            checkListenerAvailability(results[1]);
+        });
+    };
+    
+    var checkUsernameValidity = function(username) {
+        if (!username) {
+            clearField('username-validation');
+            return;
+        }
+        
+        if (usernameRegExp.test(username)) {
+            $('.username-validation').
+                removeClass('icon-cross').
+                addClass('icon-checkmark');
+    
+            $('.username-validation-message').text('This seems a valid GitHub username');
+        } else {
+            $('.username-validation').
+                removeClass('icon-checkmark').
+                addClass('icon-cross');
+    
+            $('.username-validation-message').text('Invalid username');
+            
+            extensionMessagesError.log('GitHub usernames may only contain alphanumeric<br>characters or dashes and cannot begin with a dash');
+        }
+    };
+    
+    var initFieldListeners = function() {
+        var debouncedCheckListenerAvailability = _.debounce(checkListenerAvailability, 200);
 
-    $('#closeButton').on('click', function() {
-        self.close();
+        $('#username').on('input', function() {
+            checkUsernameValidity(this.value);
+        });
+
+        $('#listener').on('input', function() {
+            if (urlRegExp.test(this.value)) {
+                debouncedCheckListenerAvailability(this.value);
+            } else {
+                clearField('listener-validation');
+            }
+        });        
+    };
+    
+    var initButtonListeners = function() {
+        $('#saveButton').on('click', function(evt) {
+            evt.preventDefault();
+    
+            chrome.runtime.sendMessage({ name: 'retrieveNotifications'});
+            
+            saveOptions();
+        });
+    
+        $('#closeButton').on('click', function() {
+            self.close();
+        });        
+    };
+    
+    $(function() {
+        initOptions();
+        initFieldListeners();
+        initButtonListeners();
     });
-});
+})();
+
+
