19bf94b4c2ba119785c034dcc5d984f4816455f0
Angelo Tata <atata@ebay.com>
2014-06-13 17:37:10 +0200

    Add GA tracking to the Popup page. Also refactor the Popup code using the Module pattern

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,40 +1,50 @@
+/*jshint unused:false */
 /*jshint sub:true */
-/*global chrome, moment, ChromeStorage */
-'use strict';
-
-var MentionRegExp = /(?:[\s])@([A-Za-z0-9]+[A-Za-z0-9-]+)/g;
-
-function initLinks() {
-    $('body').on('click', '.tag-clickable', function(evt) {
-        chrome.tabs.create({ url: $(this).data('url') });
-        evt.stopImmediatePropagation();
-        return false;
-    });
-
-    $('body').on('click', 'a', function(evt) {
-        if ($(this).attr('href')) {
-            chrome.tabs.create({ url: $(this).attr('href') });
-        }
-        
-        return false;
-    });
-}
-
-
-function prepareCommits(commits) {
-    _.each(commits, function(commit) {
-        commit.readableTimestamp = moment(commit.timestamp).fromNow();
-        commit.parsedMessage = commit.message.replace(MentionRegExp, '');
-    });
-}
-
-$(function() {
-    initLinks();
-
-    ChromeStorage.read('commits').then(function(result) {
-        prepareCommits(result.commits);
-        
-        $('.mainview').height((58 * result.commits.length) + 12);
-        $('#notifications').html(_.templates['row']({ commits: result.commits }));
-    });
-});
+/*global chrome, moment, ChromeStorage, ga, Analytics */
+var Popup = (function() {
+    'use strict';
+    
+    var MentionRegExp = /(?:[\s])@([A-Za-z0-9]+[A-Za-z0-9-]+)/g;
+    
+    var initLinks = function() {
+        $('body').on('click', '.tag-clickable', function(evt) {
+            Analytics.trackEvent('repo', 'click');
+            chrome.tabs.create({ url: $(this).data('url') });
+            evt.stopImmediatePropagation();
+            return false;
+        });
+    
+        $('body').on('click', 'a', function(evt) {
+            if ($(this).attr('href')) {
+                Analytics.trackEvent('commit', 'click');
+                chrome.tabs.create({ url: $(this).attr('href') });
+            }
+            
+            return false;
+        });
+    };
+    
+    var prepareCommits = function(commits) {
+        _.each(commits, function(commit) {
+            commit.readableTimestamp = moment(commit.timestamp).fromNow();
+            commit.parsedMessage = commit.message.replace(MentionRegExp, '');
+        });
+    };
+    
+    var initAnalytics = function() {
+        Analytics.init();
+        Analytics.trackPage('Popup');
+    };
+    
+    $(function() {
+        initAnalytics();
+        initLinks();
+    
+        ChromeStorage.read('commits').then(function(result) {
+            prepareCommits(result.commits);
+            
+            $('.mainview').height((58 * result.commits.length) + 12);
+            $('#notifications').html(_.templates['row']({ commits: result.commits }));
+        });
+    });    
+})();
