e4fb18399ab47fe40e466894b06a9aca2343b6d8
Angelo Tata <atata@ebay.com>
2014-06-14 13:54:51 +0200

    Wrap chromereload using the Module pattern

--- app/scripts/extension/chromereload.js
+++ app/scripts/extension/chromereload.js
@@ -1,22 +1,27 @@
-'use strict';
+/*jshint unused:false */
+/*global Console, chrome */
 
 // Reload client for Chrome Apps & Extensions.
 // The reload client has a compatibility with livereload.
 // WARNING: only supports reload command.
+var ChromeReload = (function() {
+    'use strict';
 
-var LIVERELOAD_HOST = 'localhost:';
-var LIVERELOAD_PORT = 35729;
-var connection = new WebSocket('ws://' + LIVERELOAD_HOST + LIVERELOAD_PORT + '/livereload');
-
-connection.onerror = function (error) {
-    Console.log('reload connection got error' + JSON.stringify(error));
-};
-
-connection.onmessage = function (e) {
-    if (e.data) {
-        var data = JSON.parse(e.data);
-        if (data && data.command === 'reload') {
-            chrome.runtime.reload();
+    var LIVERELOAD_HOST = 'localhost:',
+        LIVERELOAD_PORT = 35729,
+        connection = new WebSocket('ws://' + LIVERELOAD_HOST + LIVERELOAD_PORT + '/livereload');
+    
+    connection.onerror = function(error) {
+        Console.log('reload connection got error' + JSON.stringify(error));
+    };
+    
+    connection.onmessage = function(evt) {
+        if (evt.data) {
+            var data = JSON.parse(evt.data);
+            if (data && data.command === 'reload') {
+                chrome.runtime.reload();
+            }
         }
-    }
-};
+    };
+})();
+
